5437dd5c430bb11927e69f47d88d16f4f088c127
Maria Cecilia <cecialegre1992@gmail.com>
2023-09-01 03:15:40 -0300

    initial commit with first configurations

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'standard-with-typescript',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'eslint-config-prettier',
+	],
+	ignorePatterns: ['.eslintrc.js'],
+	settings: {
+		react: {
+			version: 'detect', // You can also specify the exact React version here
+		},
+	},
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		'no-unused-vars': 'warn',
+	},
+};
